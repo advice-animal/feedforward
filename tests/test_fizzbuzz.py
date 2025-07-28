7374e7140ba2a7f192e5c67552d33f1bdeeaace8
Tim Hatch <tim@timhatch.com>
2025-07-28 15:56:18 -0700

    Add another silly test, fizzbuzz in three parts (#4)

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,86 @@
+import feedforward
+from feedforward import Notification, State
+
+
+class FizzStep(feedforward.Step):
+    def prepare(self):
+        pass
+
+    def match(self, key):
+        if key % 3 == 0:
+            return True
+
+    def process(self, new_gen, notifications):
+        for n in notifications:
+            yield Notification(
+                key=n.key,
+                state=State(
+                    gen=self.update_generation(n.state.gen, new_gen), value="Fizz"
+                ),
+            )
+
+
+class BuzzStep(feedforward.Step):
+    def prepare(self):
+        pass
+
+    def match(self, key):
+        if key % 5 == 0:
+            return True
+
+    def process(self, new_gen, notifications):
+        for n in notifications:
+            yield Notification(
+                key=n.key,
+                state=State(
+                    gen=self.update_generation(n.state.gen, new_gen), value="Buzz"
+                ),
+            )
+
+
+class FizzBuzzStep(feedforward.Step):
+    def prepare(self):
+        pass
+
+    def match(self, key):
+        if key % 15 == 0:
+            return True
+
+    def process(self, new_gen, notifications):
+        for n in notifications:
+            yield Notification(
+                key=n.key,
+                state=State(
+                    gen=self.update_generation(n.state.gen, new_gen), value="FizzBuzz"
+                ),
+            )
+
+
+class SinkStep(feedforward.Step):
+    def prepare(self):
+        pass
+
+    def match(self, key):
+        return True
+
+    def process(self, new_gen, notifications):
+        return ()
+
+
+def test_fizzbuzz():
+    r = feedforward.Run(parallelism=2)
+    r.add_step(FizzStep())
+    r.add_step(BuzzStep())
+    r.add_step(FizzBuzzStep())
+    r.add_step(SinkStep())
+
+    results = r.run_to_completion({k: str(k) for k in range(0, 20)})
+
+    assert results[2].value == "2"
+    assert results[3].value == "Fizz"
+    assert results[5].value == "Buzz"
+    assert results[15].value == "FizzBuzz"
+
+
+if __name__ == "__main__":
+    test_fizzbuzz()
